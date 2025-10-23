b744b630d04e2c15a35211492dc88b67592b79d7
qaz-354 <3109399095@qq.com>
2025-10-23 22:43:40 +0800

    Initial commit:Qt 项目首次提交

---
+++ mainwindow.h
@@ -0,0 +1,54 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include<qstack.h>
+#include <QKeyEvent>
+#include<QMap>
+#include <QPushButton>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class MainWindow;
+}
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+    QString operand;
+    QString opcode;
+    QStack<QString> operands;
+    QStack<QString> opcodes;
+    QMap<int,QPushButton*> digitBTNs;
+
+    QString calculation(bool *ok=NULL);
+
+private slots:
+    void btnNumClicked();
+    void btnBinaryOperatorClicked();
+    void btnUnaryOperatorClicked();
+
+    void on_btnperiod_clicked();
+
+    void on_btnDel_clicked();
+
+    void on_btnClearAll_clicked();
+
+    void on_btnEqual_clicked();
+
+    void on_btnClear_clicked();
+
+    void on_btnSign_clicked();
+
+    virtual void keyPressEvent(QKeyEvent *event);
+
+private:
+    Ui::MainWindow *ui;
+};
+#endif // MAINWINDOW_H
