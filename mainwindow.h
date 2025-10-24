45bcf88c826dc544ba705f72ba3046f174615caa
qaz-354 <3109399095@qq.com>
2025-10-24 13:26:56 +0800

    Initial commit:Qt第二次提交

--- mainwindow.h
+++ mainwindow.h
@@ -21,32 +21,10 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
-    QString operand;
-    QString opcode;
-    QStack<QString> operands;
-    QStack<QString> opcodes;
-    QMap<int,QPushButton*> digitBTNs;
 
-    QString calculation(bool *ok=NULL);
 
 private slots:
-    void btnNumClicked();
-    void btnBinaryOperatorClicked();
-    void btnUnaryOperatorClicked();
 
-    void on_btnperiod_clicked();
-
-    void on_btnDel_clicked();
-
-    void on_btnClearAll_clicked();
-
-    void on_btnEqual_clicked();
-
-    void on_btnClear_clicked();
-
-    void on_btnSign_clicked();
-
-    virtual void keyPressEvent(QKeyEvent *event);
 
 private:
     Ui::MainWindow *ui;
